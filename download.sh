807628ed5c6074dc84258f4fa50b91dcf920606f
Marcus Nunes <marcus.nunes@gmail.com>
2017-04-10 23:19:48 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-wget ftp://ftp.fu-berlin.de/pub/misc/movies/database/ratings.list.gz && gzip -d ratings.list.gz
+wget ftp://ftp.fu-berlin.de/pub/misc/movies/database/ratings.list.gz && gunzip ratings.list.gz
 
 ######### Game of Thrones
 
@@ -12,3 +12,17 @@ awk 'BEGIN {FS = "#"}; {print $2}' got.dat | sed 's/)//' | sed 's/}//' | awk 'BE
 awk 'BEGIN {FS = "#"} ; {print $2}' got.dat | sed 's/)//' | sed 's/}//' | awk 'BEGIN {FS = "."} ; {print $2}' > got.episode.dat
 # rating
 awk '{print $3}' got.dat > got.rating.dat
+
+
+
+######### The Walking Dead
+
+cat ratings.list | grep "\"The Walking Dead\" (2010)" | tail -n +2 > twd.dat
+
+# season
+awk 'BEGIN {FS = "#"}; {print $2}' twd.dat | sed 's/)//' | sed 's/}//' | awk 'BEGIN {FS = "."}; {print $1}' > twd.season.dat
+# episode
+awk 'BEGIN {FS = "#"} ; {print $2}' twd.dat | sed 's/)//' | sed 's/}//' | awk 'BEGIN {FS = "."} ; {print $2}' > twd.episode.dat
+# rating
+awk '{print $3}' twd.dat > twd.rating.dat
+
