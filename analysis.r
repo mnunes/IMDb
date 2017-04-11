0c24b4270a9fa1034bfc77f212a4abce2dde2fd6
Marcus Nunes <marcus.nunes@gmail.com>
2017-04-10 23:20:27 -0300

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -1,6 +1,10 @@
 library(ggplot2)
+library(dplyr)
 
-# read the data and combine the columns
+###################
+### Game of Thrones
+
+# ler e combinar os dados
 
 got.season  <- scan(file="got.season.dat")
 got.episode <- scan(file="got.episode.dat")
@@ -8,21 +12,70 @@ got.rating  <- scan(file="got.rating.dat")
 
 got           <- cbind(got.season, got.episode, got.rating)
 got           <- data.frame(got)
-colnames(got) <- c("Temporada", "Episódio", "Rating")
+colnames(got) <- c("temporada", "episodio", "rating")
 
 ordem <- sort(got.season*100 + got.episode, index.return=TRUE)$ix
 
-got      <- got[ordem, ]
-got[, 1] <- as.character(got[, 1])
+got           <- got[ordem, ]
+got$episodio  <- 1:dim(got)[1]
+got$temporada <- as.character(got$temporada)
+
+head(got)
 
 # plots
 
-library(ggplot2)
+ggplot(got, aes(x=episodio, y=rating, color=temporada)) + 
+  labs(title="Game of Thrones: Ratings por Temporada", x="Episódio", y="Rating", colour="Temporada") + 
+  geom_smooth(method=lm, se=FALSE) + 
+  geom_point(shape=1) +
+  theme(plot.title = element_text(hjust = 0.5))
 
-#qplot(1:length(ordem), Rating, data=twentyfour, color=Season, xlab="Episode", ylab="Rating", main="24 Ratings by Season")
+got %>%
+  select(temporada, rating) %>%
+  group_by(temporada) %>%
+  summarise(media=mean(rating), mediana=median(rating), desvPad=sd(rating), maximo=max(rating), episodio_max=which.max(rating), minimo=min(rating), episodio_min=which.min(rating))
+  
+ggplot(got, aes(x=temporada, y=rating, color=temporada)) + 
+  labs(title="Game of Thrones: Ratings por Temporada", x="Temporada", y="Rating", colour="Temporada") + 
+  geom_boxplot() +
+  theme(plot.title = element_text(hjust = 0.5))
 
-Episódios = 1:length(ordem)
+####################
+### The Walking Dead
 
-ggplot(got, aes(x=Episódios, y=Rating, color=Temporada), xlab="Episódio", ylab="Rating", main="Game of Thrones Ratings por Temporada") + geom_smooth(method=lm,se=FALSE) + geom_point(shape=1, solid=TRUE)
+# ler e combinar os dados
 
-ggplot(got, aes(x=Episódios, y=Rating, color=Temporada), xlab="Episódio", ylab="Rating", main="Game of Thrones Ratings por Temporada") + geom_smooth(method=loess,se=FALSE) + geom_point(shape=1, solid=TRUE)
+twd.season  <- scan(file="twd.season.dat")
+twd.episode <- scan(file="twd.episode.dat")
+twd.rating  <- scan(file="twd.rating.dat")
+
+twd           <- cbind(twd.season, twd.episode, twd.rating)
+twd           <- data.frame(twd)
+colnames(twd) <- c("temporada", "episodio", "rating")
+
+ordem <- sort(twd.season*100 + twd.episode, index.return=TRUE)$ix
+
+twd           <- twd[ordem, ]
+twd$episodio  <- 1:dim(twd)[1]
+twd$temporada <- as.character(twd$temporada)
+
+head(twd)
+
+# plots
+
+ggplot(twd, aes(x=episodio, y=rating, color=temporada)) + 
+  labs(title="The Walking Dead: Ratings por Temporada", x="Episódio", y="Rating", colour="Temporada") + 
+  geom_smooth(method=lm, se=FALSE) + 
+  geom_point(shape=1) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+twd %>%
+  select(temporada, rating) %>%
+  group_by(temporada) %>%
+  summarise(media=mean(rating), mediana=median(rating), desvPad=sd(rating), maximo=max(rating), episodio_max=which.max(rating), minimo=min(rating), episodio_min=which.min(rating))
+
+ggplot(twd, aes(x=temporada, y=rating, color=temporada)) + 
+  labs(title="The Walking Dead: Ratings por Temporada", x="Temporada", y="Rating", colour="Temporada") + 
+  geom_boxplot() +
+  theme(plot.title = element_text(hjust = 0.5))
+
